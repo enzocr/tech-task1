230b5ca7b5910e7bc8a7014f6dd60aa96a5e3bb6
enzocr <enzoquartino@gmail.com>
2025-02-04 15:57:50 -0600

    [TECH-TASK][FIRST-TECH-TASK] Improving exception handling

--- src/main/java/com/cenfo/tech/task1/response/LogInResponse.java
+++ src/main/java/com/cenfo/tech/task1/response/LogInResponse.java
@@ -1,15 +1,6 @@
 package com.cenfo.tech.task1.response;
 
-import com.cenfo.tech.task1.entity.User;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
+import lombok.Builder;
 
-@Data
-public class LogInResponse {
-    private String token;
-
-    @JsonIgnore
-    private User authUser;
-
-    private long expiresIn;
-}
+@Builder
+public record LogInResponse(String token, long expiresIn) {}
